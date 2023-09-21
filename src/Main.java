a0df904a9f66024c6559b3c83457385af6b0bf22
VictorLeopoldo <victorleovazavi@gmail.com>
2023-09-21 08:21:02 -0300

    Imprimindo Hello Word

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    System.out.println("Hello word !");//comando para imprimir o resultado para o usuário.
     }
 }
